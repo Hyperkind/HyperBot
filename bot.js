c3b740854a221f9e28cac556d1df4e0d38db9996
Jon Ah-Nee <jonahnee@gmail.com>
2016-02-07 16:18:42 -1000

    got my first command working

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var express = require('express');
+// var express = require('express');
 var CONFIG = require('./config');
 var IRC = require('irc');
 
@@ -6,26 +6,27 @@ var IRC = require('irc');
 
 var bot = new IRC.Client(CONFIG.SERVER, CONFIG.USERNAME,
   {
-    channels : [CONFIG.CHANNELS],
+    channels : CONFIG.CHANNELS,
     debug : false,
     password : CONFIG.PASSWORD,
     username : CONFIG.USERNAME
   });
 
+// Confirms that the bot has connected to the channel
 bot.addListener('join', function (channel, who) {
-  if(who == CONFIG.USERNAME){
-    console.log(who + ' recognized as joined.');
+  if (who === CONFIG.USERNAME) {
+    console.log(who + ' has joined on channel ' + channel);
   } else {
-    console.log(who + ' joined the chat!');
-    var joinMes = 'Welcome to the channel, ' + who + '!';
-    bot.say(CONFIG.CHANNELS[0], joinMes);
+    console.log(who + ' has joined on channel ' + channel);
+    var greetMsg = 'Welcome to the channel ';
+    bot.say(CONFIG.CHANNELS[0], greetMsg + who);
   }
 });
 
-bot.addListener('part', function (channel, who) {
+bot.addListener('part', function (channel, message, who) {
   console.log(who + ' left the chat!');
-  var leaveMes = 'Take it easy, ' + who + '!';
-  bot.say(CONFIG.CHANNELS[0], leaveMes);
+  var leaveMsg = 'Take it easy, ' + who + '!';
+  bot.say(CONFIG.CHANNELS, leaveMsg);
 });
 
 // listens for messages to the channel and displays in terminal
@@ -33,4 +34,10 @@ bot.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
 });
 
-bot.connect();
+// !hello command
+bot.addListener('message', function (from, to, message) {
+  if (message === '!hello') {
+    var helloMsg = "Is it me you're looking for?";
+    bot.say(CONFIG.CHANNELS[0], helloMsg);
+  }
+});
